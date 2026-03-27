a201abd1a53db74cfd665fb9f1230ff1cf4cc5da
brahmantiodiaz <auliabrahmantio@gmail.com>
2026-03-28 02:49:05 +0700

    initial commit

---
+++ js/components/footer.js
@@ -0,0 +1,7 @@
+components.footer = function () {
+  document.getElementById("footer").innerHTML = `
+    <footer class="text-center footer py-4">
+      Copyright © Hactiv8 Batch 95 ${new Date().getFullYear()}. All Rights Reserved
+    </footer>
+  `;
+};
