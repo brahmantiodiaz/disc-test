5e24de285ae138d41e70f5ed1707719a4070e1a6
Aulia Brahmantio Diaz <55686668+brahmantiodiaz@users.noreply.github.com>
2026-05-19 23:44:09 +0700

    fix: harden permission guards

--- js/core/permission.js
+++ js/core/permission.js
@@ -1,12 +1,6 @@
 let permissions = {
   requireCurrentParticipant: function () {
     if (!localStorage.getItem("disc_current_participant")) {
-      // services.alert.warning(
-      //   "Isi data peserta dulu sebelum masuk ke halaman test.",
-      // );
-      // setTimeout(function () {
-      //   window.location.href = "user-form.html";
-      // }, 800);
       return false;
     }
     return true;
@@ -14,17 +8,8 @@ let permissions = {
 
   requireAdmin: function () {
     let user = services.storage.get("userLogin", {});
-    console.log(user.isAdmin);
 
-    if (!user.isAdmin && user.userName) {
-      services.alert.warning("Page ini hanya bisa di akses oleh admin.");
-
-      setTimeout(function () {
-        window.location.href = "index.html";
-      }, 900);
-
-      return false;
-    } else if (!user.userName) {
+    if (!user.userName) {
       services.alert.warning("Login admin terlebih dahulu.");
 
       setTimeout(function () {
@@ -34,33 +19,45 @@ let permissions = {
       return false;
     }
 
+    if (!user.isAdmin) {
+      services.alert.warning("Page ini hanya bisa di akses oleh admin.");
+
+      setTimeout(function () {
+        window.location.href = "index.html";
+      }, 900);
+
+      return false;
+    }
+
     return true;
   },
 
   isAdmin: function () {
-    let user = services.storage.get("userLogin");
-
-    return user.isAdmin;
+    let user = services.storage.get("userLogin", {});
+    return !!user.isAdmin;
   },
 
   preventAdminLoginPageIfLoggedIn: function () {
     let user = services.storage.get("userLogin", {});
+
     if (user.isAdmin) {
       window.location.href = "admin-dashboard.html";
       return false;
     }
-    if (user && user.isAdmin === false) {
+
+    if (user.userName && user.isAdmin === false) {
       window.location.href = "index.html";
       return false;
     }
+
     return true;
   },
-  requireUser: function () {
-    let userName = userData();
-    console.log(userName.userName);
 
-    if (!userName.userName || userName.isAdmin) {
-      if (userName.isAdmin) {
+  requireUser: function () {
+    let user = userData();
+
+    if (!user.userName || user.isAdmin) {
+      if (user.isAdmin) {
         services.alert.warning("Admin tidak bisa melakukan assessment.");
       } else {
         services.alert.warning("User harus login terlebih dahulu.");
